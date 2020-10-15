6a951f119dead669a619df2ea59ac9bad2f44bc1
laineus <lainen000@gmail.com>
2020-10-15 22:52:58 +0900

    image

---
+++ src/components/Image.vue
@@ -0,0 +1,29 @@
+<template>
+  <div />
+</template>
+
+<script>
+import { inject } from 'vue'
+import { initGameObject } from '../index.js'
+export default {
+  setup (props, context) {
+    const scene = inject('scene')
+    class Image extends Phaser.GameObjects.Image {
+      preUpdate () {}
+    }
+    const object = new Image(scene, props.x, props.y, props.texture)
+    initGameObject(object, props, context)
+    return { object }
+  },
+  props: [
+    'visible',
+    'x', 'y',
+    'origin', 'originX', 'originY',
+    'scale', 'scaleX', 'scaleY',
+    'depth',
+    'alpha', 'blendMode',
+    'texture', 'frame',
+    'flipX', 'flipY'
+  ]
+}
+</script>
