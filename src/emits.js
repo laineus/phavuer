d5a06700e9086c9dca4339c58d2cb0a93b189be7
Laineus <lainen000@gmail.com>
2022-12-06 18:30:04 +0900

    fix emit warn

--- src/emits.js
+++ src/emits.js
@@ -1,3 +1,3 @@
 import { GAME_OBJECT_EVENTS } from './setters.js'
 const emits = GAME_OBJECT_EVENTS.map(v => v.emit)
-export const gameObjectEmits = ['create', ...emits]
+export const gameObjectEmits = ['create', ...emits, 'update:x', 'update:y']
