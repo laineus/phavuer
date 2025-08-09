3dbfdb2567d3f5c3cd7b72ddd2afb30acd6f0908
Dennis Smuda <smuda.dennis@gmail.com>
2025-08-09 19:54:35 +0200

    chore: delete before reinit

--- src/emits.js
+++
@@ -1,4 +0,0 @@
-import { GAME_OBJECT_EVENTS, vModelProps } from './setters.js'
-const emits = GAME_OBJECT_EVENTS.map(v => v.emit)
-const vModelEmits = vModelProps.map(key => `update:${key}`)
-export const gameObjectEmits = ['create', ...emits, ...vModelEmits]
