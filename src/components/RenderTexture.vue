647c22f3840d409ce3c6d4949d4f591c821c7aa8
Laineus <lainen000@gmail.com>
2026-05-01 09:58:06 +0900

    fix RenderTexture

--- src/components/RenderTexture.vue
+++ src/components/RenderTexture.vue
@@ -1,6 +1,6 @@
 <script>
 import * as Phaser from 'phaser'
-import { defineComponent, inject, onMounted, onUpdated, provide } from 'vue'
+import { defineComponent, inject, nextTick, onMounted, onUpdated, provide } from 'vue'
 import { gameObjectEmits } from '../emits.js'
 import { initGameObject, InjectionKeys } from '../index.js'
 import { gameObjectProps, mapProps } from '../props.js'
@@ -21,11 +21,18 @@ export default defineComponent({
     const renderList = []
     provide(InjectionKeys.RenderTextureRenderList, renderList)
     provide(InjectionKeys.GameObject, object)
-    const render = () => {
-      object.beginDraw()
-      renderList.forEach(v => object.batchDraw(v))
-      object.endDraw()
-    }
+    const render = object.beginDraw
+      // Phaser v3
+      ? () => {
+          object.beginDraw()
+          renderList.forEach(v => object.batchDraw(v))
+          object.endDraw()
+        }
+      // Phaser v4
+      : () => {
+          renderList.forEach(v => object.draw(v))
+          object.render()
+        }
     onMounted(() => {
       render()
     })
